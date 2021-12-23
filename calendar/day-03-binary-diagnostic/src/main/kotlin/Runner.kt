2891d124ed8bc64f95aff9d28564e093b244757c
Martin Kansik <martin.kansik@genuitek.com>
2021-12-23 23:27:07 +0100

    Day 3: Part II

--- calendar/day-03-binary-diagnostic/src/main/kotlin/Runner.kt
+++ calendar/day-03-binary-diagnostic/src/main/kotlin/Runner.kt
@@ -1,23 +1,57 @@
-import models.Rates
+import models.LifeSupportRates
+import models.PowerRates
 import kotlin.io.path.Path
+import kotlin.math.ceil
 
 object Runner {
     @JvmStatic
     fun main(args: Array<String>) {
         val binaries = Utils.readFile(Path("calendar/day-03-binary-diagnostic/src/main/resources/data.txt"))
-        val rates = calculateRates(binaries)
 
-        println("Gamma rate: ${rates.gammaRate.toInt(2)}, epsilon rate: ${rates.epsilonRate.toInt(2)}. " +
-                "Result: ${rates.gammaRate.toInt(2) * rates.epsilonRate.toInt(2)}")
+        val powerRates = calculatePowerRates(binaries)
+        println("Gamma rate: ${powerRates.gammaRate.toInt(2)}, epsilon rate: ${powerRates.epsilonRate.toInt(2)}. " +
+                "Result: ${powerRates.gammaRate.toInt(2) * powerRates.epsilonRate.toInt(2)}")
+
+        val lifeSupportRates = calculateLifeSupportRates(binaries)
+        println("Oxygen rate: ${lifeSupportRates.oxygenGeneratorRate.toInt(2)}, CO2 rate: ${lifeSupportRates.co2ScrubberRate.toInt(2)}. " +
+                "Result: ${lifeSupportRates.oxygenGeneratorRate.toInt(2) * lifeSupportRates.co2ScrubberRate.toInt(2)}")
     }
 
-    private fun calculateRates(binaries: List<String>): Rates {
+    private fun calculatePowerRates(binaries: List<String>): PowerRates {
         val oneCounter = MutableList(binaries[0].length) {0}
         binaries.forEach { it.forEachIndexed { index, c -> if(c == '1') oneCounter[index]++ } }
 
         val gammaRate: List<Byte> = oneCounter.map { if (it > binaries.size/2) 1 else 0 }
         val epsilonRate: List<Byte> = oneCounter.map { if (it < binaries.size/2) 1 else 0 }
 
-        return Rates(gammaRate.joinToString(""), epsilonRate.joinToString(""))
+        return PowerRates(gammaRate.joinToString(""), epsilonRate.joinToString(""))
+    }
+
+    private fun calculateLifeSupportRates(binaries: List<String>): LifeSupportRates =
+        LifeSupportRates(getLifeSupportRate(binaries, true), getLifeSupportRate(binaries, false))
+
+    private fun getLifeSupportRate(binaries: List<String>, isOxygen: Boolean): String {
+        var temp = binaries.map { it }
+        var idx = 0
+
+        do {
+            val determinedChar = determineCharAtIndex(temp, idx, isOxygen)
+            temp = temp.filter { it[idx] == determinedChar }
+            idx++
+        } while (idx < binaries[0].length && temp.size != 1)
+
+        return temp[0]
+    }
+
+
+    private fun determineCharAtIndex(binaries: List<String>, idx: Int, isOxygen: Boolean): Char {
+        var counter = 0
+        binaries.forEach { if (it[idx] == '1') counter++ }
+        val condition = counter >= ceil(binaries.size/2f)
+        return if(isOxygen) {
+            if (condition) '1' else '0'
+        } else {
+            if (condition) '0' else '1'
+        }
     }
 }
--- calendar/day-03-binary-diagnostic/src/main/kotlin/models/Rates.kt
+++ calendar/day-03-binary-diagnostic/src/main/kotlin/models/Rates.kt
@@ -1,3 +1,5 @@
 package models
 
-data class Rates(val gammaRate: String, val epsilonRate: String)
+data class PowerRates(val gammaRate: String, val epsilonRate: String)
+
+data class LifeSupportRates(val oxygenGeneratorRate: String, val co2ScrubberRate: String)
