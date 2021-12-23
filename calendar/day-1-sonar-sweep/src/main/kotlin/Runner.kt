7d04d88a8b1fe2c8ec33997ab69edd5caa851adc
Martin Kansik <martin.kansik@genuitek.com>
2021-12-23 19:02:45 +0100

    Day 1: Part II

--- calendar/day-1-sonar-sweep/src/main/kotlin/Runner.kt
+++ calendar/day-1-sonar-sweep/src/main/kotlin/Runner.kt
@@ -3,14 +3,28 @@ import kotlin.io.path.Path
 object Runner {
     @JvmStatic
     fun main(args: Array<String>) {
-        val lines = FileReader.readFile(Path("calendar/day-1-sonar-sweep/src/main/resources/data.txt"))
-        val result = countLargerMeasurements(lines)
-        println(result)
+        val measurements = FileReader.readFile(Path("calendar/day-1-sonar-sweep/src/main/resources/data.txt"))
+            .map { it.toInt() }
+
+        val result = countLargerMeasurements(measurements)
+        println("Result for part I: $result")
+
+
+        val resultSliderWindow = countLargerMeasurementsSliderWindow(measurements, 3)
+        println("Result for part II: $resultSliderWindow")
     }
     
-    private fun countLargerMeasurements(lines: List<String>): Int = lines.map{ it.toInt() }
+    private fun countLargerMeasurements(measurements: List<Int>): Int = measurements
         .foldIndexed(0) { idx, acc, element ->
-            if (idx < 1 || element <= lines[idx-1].toInt()) acc
+            if (idx < 1 || element <= measurements[idx-1]) acc
+            else acc + 1
+        }
+
+    private fun countLargerMeasurementsSliderWindow(measurements: List<Int>, windowSize: Int): Int = measurements
+        .foldIndexed(0) { idx, acc, _ ->
+            if (idx < windowSize
+                || measurements.subList(idx - windowSize + 1, idx + 1).sum() <= measurements.subList(idx - windowSize, idx).sum()
+            ) acc
             else acc + 1
         }
 }
