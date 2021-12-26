5bdc3e3573ae581bfdcf2c34df4306d09cb0e6d0
Martin Kansik <martin.kansik@genuitek.com>
2021-12-26 21:52:32 +0100

    Day 5: Part II

--- calendar/day-05-hydrothermal-venture/src/main/kotlin/Runner.kt
+++ calendar/day-05-hydrothermal-venture/src/main/kotlin/Runner.kt
@@ -14,31 +14,29 @@ object Runner {
                 val s2 = it[1].split(',')
                 Line(Point(s1[0].toInt(), s1[1].toInt()), Point(s2[0].toInt(), s2[1].toInt()))
             }
-        val overlappedPoints = getOverlappedPointsBy(lines)
+        val overlappedPoints = getOverlappedPointsBy(lines, false)
         val pointsOverlappedMoreThanTwice = countPointsOverlappedMoreOrEqualThan(2, overlappedPoints)
         println("Points do at least two lines overlap $pointsOverlappedMoreThanTwice")
+
+        val overlappedPointsWithDiagonals = getOverlappedPointsBy(lines, true)
+        val pointsOverlappedMoreThanTwiceWithDiagonals = countPointsOverlappedMoreOrEqualThan(2, overlappedPointsWithDiagonals)
+        println("Points do at least two lines overlap with diagonals $pointsOverlappedMoreThanTwiceWithDiagonals")
     }
 
-    private fun getOverlappedPointsBy(lines: List<Line>): List<OverlappingPoint> {
-        val overlappingPoints = mutableListOf<OverlappingPoint>()
-        lines.forEach {
-            val pointsInLine = if (it.isHorizontal || it.isVertical) drawLine(it) else emptyList()
-            pointsInLine.forEach { point ->
-                val foundPoint = overlappingPoints.find { d -> d == point }
-                if(foundPoint != null) foundPoint.overlapping++
-                else overlappingPoints.add(OverlappingPoint(point.x, point.y, 1))
-            }
-        }
-        return overlappingPoints
+    private fun getOverlappedPointsBy(lines: List<Line>, countDiagonals: Boolean): List<OverlappingPoint> {
+        val points = mutableListOf<Point>()
+        lines.forEach { points.addAll(drawLine(it, countDiagonals)) }
+        val grouped = points.groupBy { it }
+        return grouped.keys.map { OverlappingPoint(it.x, it.y, grouped[it]!!.size) }
     }
 
-    private fun drawLine(line: Line): List<Point> = if (line.isVertical) {
+    private fun drawLine(line: Line, countDiagonals: Boolean): List<Point> = if (line.isVertical) {
         drawVerticalLine(line)
     } else if (line.isHorizontal) {
         drawHorizontalLine(line)
-    } else {
-        emptyList()
-    }
+    } else if (countDiagonals) {
+        drawDiagonalLine(line)
+    } else emptyList()
 
     private fun drawHorizontalLine(line: Line): List<Point> =
         if (line.p1.x < line.p2.x) (line.p1.x..line.p2.x).map { Point(it, line.p1.y) }
@@ -47,6 +45,41 @@ object Runner {
     private fun drawVerticalLine(line: Line): List<Point> =
         if(line.p1.y < line.p2.y) (line.p1.y..line.p2.y).map { Point(line.p1.x, it) }
         else (line.p2.y..line.p1.y).map { Point(line.p1.x, it) }
+
+    private fun drawDiagonalLine(line: Line): List<Point> {
+        val xInc: Int
+        val yInc: Int
+        if (line.p1.x < line.p2.x) {
+            if(line.p1.y < line.p2.y) {
+                xInc = 1
+                yInc = 1
+            } else {
+                xInc = 1
+                yInc = -1
+            }
+        } else {
+            if(line.p1.y < line.p2.y) {
+                xInc = -1
+                yInc = 1
+            } else {
+                xInc = -1
+                yInc = -1
+            }
+        }
+        val points = mutableListOf(line.p1)
+        var x = line.p1.x
+        var y = line.p1.y
+
+        do {
+            x += xInc
+            y += yInc
+            points.add(Point(x, y))
+        } while (x != line.p2.x && y != line.p2.y)
+
+        return points
+    }
+
+
 
     private fun countPointsOverlappedMoreOrEqualThan(times: Int, points: List<OverlappingPoint>): Int
         = points.count { it.overlapping >= times }
