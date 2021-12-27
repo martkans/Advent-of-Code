5a107876b57e1007c6a019be6a052f3d6b1aa6fd
Martin Kansik <martin.kansik@genuitek.com>
2021-12-27 19:09:16 +0100

    Day 7: Part II

--- calendar/day-07-the-treachery-of-whales/src/main/kotlin/Runner.kt
+++ calendar/day-07-the-treachery-of-whales/src/main/kotlin/Runner.kt
@@ -8,19 +8,27 @@ object Runner {
             .split(',').map { it.toInt() }
 
         val minFuel = findPositionWithLeastFuelAmount(crabSubmarinesPositions)
-
         println("Minimal amount of fuel to set crab submarines on specific position: $minFuel")
+
+        val minFuelAdvanced = findPositionWithLeastFuelAmount(crabSubmarinesPositions, true)
+        println("Minimal amount of fuel with advanced consumption to set crab submarines on specific position: $minFuelAdvanced")
     }
 
-    private fun findPositionWithLeastFuelAmount(initPositions: List<Int>): Int? {
+    private fun findPositionWithLeastFuelAmount(initPositions: List<Int>, advancedFuelConsumption: Boolean = false): Int? {
         val min = initPositions.minOrNull()
         val max = initPositions.maxOrNull()
 
         return (min!!..max!!).map {
-            calculateAmountOfFuelNeededToMoveToPosition(it, initPositions)
+            calculateAmountOfFuelNeededToMoveToPosition(it, initPositions, advancedFuelConsumption)
         }.minOrNull()
     }
 
-    private fun calculateAmountOfFuelNeededToMoveToPosition(position: Int, initPositions: List<Int>) =
-        initPositions.sumOf { abs(it - position) }
+    private fun calculateAmountOfFuelNeededToMoveToPosition(position: Int, initPositions: List<Int>,
+                                                            advancedFuelConsumption: Boolean) =
+        initPositions.sumOf {
+            if(advancedFuelConsumption) calculateAdvancedFuelConsumption(position, it) else abs(it - position)
+        }
+
+    private fun calculateAdvancedFuelConsumption(destination: Int, initPosition: Int) =
+        (1..abs(destination - initPosition)).sum()
 }
