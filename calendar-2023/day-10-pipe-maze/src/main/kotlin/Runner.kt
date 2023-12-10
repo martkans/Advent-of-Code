1cd891d8b4517eb6cd6db74cd82e61351bda524c
Martin Kansik <martkans@outlook.com>
2023-12-10 12:27:10 +0100

    Calendar 2023: Day 10 I refactor

--- calendar-2023/day-10-pipe-maze/src/main/kotlin/Runner.kt
+++ calendar-2023/day-10-pipe-maze/src/main/kotlin/Runner.kt
@@ -5,7 +5,7 @@ object Runner {
     fun main(args: Array<String>) {
         val maze = Utils.readFile(Path("calendar-2023/day-10-pipe-maze/src/main/resources/data.txt"))
 
-        println("Result for I: ${maze.findCycle().size/2}")
+        println("Result for I: ${maze.findCycle().size / 2}")
     }
 
     private val UP_POSSIBILITIES = setOf('7', '|', 'F')
@@ -14,62 +14,50 @@ object Runner {
     private val LEFT_POSSIBILITIES = setOf('L', '-', 'F')
 
     private fun List<String>.findCycle(): List<Char> {
-        var (prev, next, nextDirection, i, j) = findStartWithSymbol()
+        var (prev, next, nextIncomingDirection, i, j) = findStartWithSymbol()
         val path = mutableListOf(prev)
 
         while (next != 'S') {
-            val nextIncoming = when(nextDirection) {
-                DIRECTION.DOWN -> DIRECTION.UP
-                DIRECTION.RIGHT -> DIRECTION.LEFT
-                DIRECTION.LEFT -> DIRECTION.RIGHT
-                DIRECTION.UP -> DIRECTION.DOWN
-            }
 
             path.add(next)
 
-            when(next) {
-                '|' -> if (nextIncoming == DIRECTION.DOWN) {
-                    i -= 1
-                    nextDirection = DIRECTION.UP
-                } else  {
+            when (next) {
+                '|' -> if (nextIncomingDirection == DIRECTION.DOWN) i -= 1 else i += 1
+
+                '-' -> if (nextIncomingDirection == DIRECTION.RIGHT) j -= 1 else j += 1
+
+                '7' -> if (nextIncomingDirection == DIRECTION.LEFT) {
                     i += 1
-                    nextDirection = DIRECTION.DOWN
-                }
-                '-' -> if (nextIncoming == DIRECTION.RIGHT) {
-                    j -= 1
-                    nextDirection = DIRECTION.LEFT
-                } else {
-                    j += 1
-                    nextDirection = DIRECTION.RIGHT
-                }
-                '7' -> if (nextIncoming == DIRECTION.LEFT) {
-                    i += 1
-                    nextDirection = DIRECTION.DOWN
+                    nextIncomingDirection = DIRECTION.UP
                 } else {
                     j -= 1
-                    nextDirection = DIRECTION.LEFT
+                    nextIncomingDirection = DIRECTION.RIGHT
                 }
-                'F' -> if (nextIncoming == DIRECTION.RIGHT) {
+
+                'F' -> if (nextIncomingDirection == DIRECTION.RIGHT) {
                     i += 1
-                    nextDirection = DIRECTION.DOWN
+                    nextIncomingDirection = DIRECTION.UP
                 } else {
                     j += 1
-                    nextDirection = DIRECTION.RIGHT
+                    nextIncomingDirection = DIRECTION.LEFT
                 }
-                'J' -> if (nextIncoming == DIRECTION.LEFT) {
+
+                'J' -> if (nextIncomingDirection == DIRECTION.LEFT) {
                     i -= 1
-                    nextDirection = DIRECTION.UP
+                    nextIncomingDirection = DIRECTION.DOWN
                 } else {
                     j -= 1
-                    nextDirection = DIRECTION.LEFT
+                    nextIncomingDirection = DIRECTION.RIGHT
                 }
-                'L' -> if (nextIncoming == DIRECTION.RIGHT) {
+
+                'L' -> if (nextIncomingDirection == DIRECTION.RIGHT) {
                     i -= 1
-                    nextDirection = DIRECTION.UP
+                    nextIncomingDirection = DIRECTION.DOWN
                 } else {
                     j += 1
-                    nextDirection = DIRECTION.RIGHT
+                    nextIncomingDirection = DIRECTION.LEFT
                 }
+
                 else -> error("should not happen")
             }
 
@@ -93,12 +81,12 @@ object Runner {
                         if (it > 0 && LEFT_POSSIBILITIES.contains(this[i][it - 1])) this[i][it - 1] else null // LEFT
 
                     return when {
-                        up != null && down != null -> NextStep('|', up, DIRECTION.UP, i -1, it)
-                        up != null && right != null -> NextStep('L', up, DIRECTION.UP, i -1, it)
-                        up != null && left != null -> NextStep('J', up, DIRECTION.UP, i -1, it)
-                        left != null && right != null -> NextStep('-' , left, DIRECTION.LEFT, i, it - 1)
-                        left != null && down != null -> NextStep('7', left, DIRECTION.LEFT, i, it - 1)
-                        right != null && down != null -> NextStep('F', right, DIRECTION.RIGHT, i, it + 1)
+                        up != null && down != null -> NextStep('|', up, DIRECTION.DOWN, i - 1, it)
+                        up != null && right != null -> NextStep('L', up, DIRECTION.DOWN, i - 1, it)
+                        up != null && left != null -> NextStep('J', up, DIRECTION.DOWN, i - 1, it)
+                        left != null && right != null -> NextStep('-', left, DIRECTION.RIGHT, i, it - 1)
+                        left != null && down != null -> NextStep('7', left, DIRECTION.RIGHT, i, it - 1)
+                        right != null && down != null -> NextStep('F', right, DIRECTION.LEFT, i, it + 1)
                         else -> error("should not happen")
                     }
                 }
@@ -115,10 +103,8 @@ object Runner {
     data class NextStep(
         val prev: Char,
         val nextSymbol: Char,
-        val nextDirection: DIRECTION,
+        val nextIncomingDirection: DIRECTION,
         val i: Int,
         val j: Int
-    ) {
-
-    }
+    )
 }
