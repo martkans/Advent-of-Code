9750838b1ebc628bb958ad7bb7751f1f80a14737
Martin Kansik <martkans@outlook.com>
2023-12-12 23:18:57 +0100

    Calendar 2023: Day 12 I improvements

--- calendar-2023/day-12-hot-springs/src/main/kotlin/Runner.kt
+++ calendar-2023/day-12-hot-springs/src/main/kotlin/Runner.kt
@@ -1,14 +1,28 @@
 import kotlin.io.path.Path
+import kotlin.system.measureTimeMillis
 import kotlin.text.StringBuilder
 
 object Runner {
-//    NOT BEST SOLUTION
+    //    NOT BEST SOLUTION
     @JvmStatic
     fun main(args: Array<String>) {
         val data = Utils.readFile(Path("calendar-2023/day-12-hot-springs/src/main/resources/data.txt"))
         val conditionRecords = extractData(data)
 
-        println("Result for I: ${conditionRecords.sumOf { it.countPossibleConfigurations() }}")
+        val a = measureTimeMillis {
+            println("Result for I: ${conditionRecords.sumOf { it.createAndCountConfigurations() }}")
+        }
+
+        val b = measureTimeMillis {
+            println("Result for I: ${conditionRecords.sumOf { it.countPossibleConfigurations() }}")
+        }
+
+        // nice comparison
+        println("Time for optimized method: $a ms")
+        println("Time for not optimized method: $b ms")
+
+        val extendedRecords = conditionRecords.extendData()
+
     }
 
     data class ConditionRecord(
@@ -21,7 +35,19 @@ object Runner {
 
         private fun checkIfConfigurationPassRequirements(configuration: String): Boolean =
             BROKEN.findAll(configuration).toList().let {
-                it.size == contiguousGroup.size && it.zip(contiguousGroup).all { (it.first.range.last - it.first.range.first + 1)  == it.second }
+                it.size == contiguousGroup.size && it.zip(contiguousGroup)
+                    .all { (it.first.range.last - it.first.range.first + 1) == it.second }
+            }
+
+        private fun checkIfConfigurationPassRequirementsPartially(configuration: String): Boolean =
+            BROKEN.findAll(configuration).toList().let {
+                with(it.zip(contiguousGroup)) {
+                    if (this.isEmpty()) {
+                        true
+                    } else if (this.last().first.range.last - this.last().first.range.first + 1 > this.last().second)
+                        false
+                    else this.dropLast(1).all { (it.first.range.last - it.first.range.first + 1) == it.second }
+                }
             }
 
 
@@ -39,6 +65,25 @@ object Runner {
             ).flatten()
         }
 
+        fun createAndCountConfigurations(a: String = ""): Int {
+            var idx = a.length
+            val b = StringBuilder(a)
+            while (idx < condition.length && condition[idx] != '?') {
+                b.append(condition[idx])
+                idx += 1
+            }
+
+            if (idx == condition.length) {
+                return if (checkIfConfigurationPassRequirements(b.toString())) 1 else 0
+            }
+
+            val dotSegment =
+                if (checkIfConfigurationPassRequirementsPartially("$b.")) createAndCountConfigurations("$b.") else 0
+            val hashSegment =
+                if (checkIfConfigurationPassRequirementsPartially("$b#")) createAndCountConfigurations("$b#") else 0
+            return dotSegment + hashSegment
+        }
+
     }
 
     private fun extractData(data: List<String>) = data.map { line ->
@@ -48,6 +93,13 @@ object Runner {
                 contiguousGroup = it.last().split(",").map { it.toInt() }
             )
         }
+    }
+
+    private fun List<ConditionRecord>.extendData(multiplier: Int = 5) = this.map { record ->
+        ConditionRecord(
+            condition = (List(multiplier) { record.condition }).joinToString("?"),
+            contiguousGroup = (List(multiplier) { record.contiguousGroup }).flatten()
+        )
     }
 
 }
